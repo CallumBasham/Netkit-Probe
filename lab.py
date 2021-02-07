b881f7e2e98b10a216e112f5e61af9c260809a77
hex <hex@hex.com>
2021-02-07 17:31:53 +0000

    Created control over netkit labs, began probing for NK data

--- lab.py
+++ lab.py
@@ -1,5 +1,15 @@
 import os
+import subprocess
+import io
 
+
+class MachineData:
+    def __init__(self, name):
+        self.machineName = name
+        self.machineConnections = []
+
+    def addConnection(self, ethDev, wireName):
+        self.machineConnections.append((ethDev, wireName))
 
 class NetkitLab:
 
@@ -35,10 +45,44 @@ class NetkitLab:
         os.system('cd ' + self.labDirectory + ' && ~/netkit-jh/bin/lstart')
 
     def stopLab(self):
-        os.system('cd ' + self.labDirectory + ' && ~/netkit-jh/bin/lcrash')
+        os.system('cd ' + self.labDirectory + ' && lcrash')
+
+    def getMachineInfo(self, line):
+        # Dump list into array and clear spaces
+        lineSplit = line.split(' ')
+        lineSplit = list(filter(None, lineSplit))
+
+        # Create new Machine Object with its Name
+        nk = MachineData(lineSplit[1])
+
+        # Browse the string for the Machines networking data
+        for i in range(0, len(lineSplit), 1):
+            if lineSplit[i] == "@":
+                nk.addConnection(lineSplit[i - 1], lineSplit[i + 1])
+
+        # Return the Machine object
+        return nk
+
 
     def probeLab(self):
-        os.system('cd ' + self.labDirectory + ' && ~/netkit-jh/bin/linfo')
+        wid = 10
+        for i in self.machineList:
+            os.system('xdotool search --name "' + i + '"  windowactivate windowmove -- ' + str(
+                wid) + '  300 windowsize 450 450 type " ping localhost "')
+          #  for x in range(0, 500, 25):
+            #    os.system('xdotool search --name "' + i + '"  windowactivate windowsize ' + str(x) + ' ' + str(x) + ' ')
+            os.system('xdotool search --name "' + i + '" windowactivate key Return')
+            wid = wid + 100
+
+        proc = subprocess.Popen("cd " + self.labDirectory + " && vlist", stdout=subprocess.PIPE, shell=True)
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            if "@" in line and len(line) > 5:
+
+                newMachine = self.getMachineInfo(line)
+
+
+
+
 
     # Constructor
     # Create a NetkitLab class based on a selected lab.conf File
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from lab import NetkitLab
 
 # Overall UI -->-->-->-->-->-->-->-->-->-->
 root = Tk()
+#root.attributes("-fullscreen", True)
+root.attributes("-zoomed", True)
 root.title("Netkit Probe")
 
 # Base frame -->-->-->-->-->-->-->-->-->-->
@@ -17,18 +19,28 @@ def addLabButtons(nlab):
     machineList = Label(base, anchor=W)
     machineList.grid(row=1, column=1, columnspan=1)
 
-    startLabBtn = Button(base, text="Start Lab", state=NORMAL, command=nlab.startLab)
+    startLabBtn = Button(base, text="Start Lab", state=NORMAL, command=lambda: btnStartLab(nlab))
     startLabBtn.grid(row=1, column=2, columnspan=1)
 
-    stopLabBtn = Button(base, text="Stop Lab", state=NORMAL, command=nlab.stopLab)
+    stopLabBtn = Button(base, text="Stop Lab", state=NORMAL, command=lambda: btnStopLab(nlab))
     stopLabBtn.grid(row=1, column=3, columnspan=1)
 
-    probeLabBtn = Button(base, text="Probe Lab", state=NORMAL, command=nlab.probeLab)
+    probeLabBtn = Button(base, text="Probe Lab", state=NORMAL, command=lambda: btnProbeLab(nlab))
     probeLabBtn.grid(row=1, column=4, columnspan=1)
 
     machineList["text"] = "Machines: " + ' '.join(nlab.machineList)
 
+def btnStartLab(nlab):
+    nlab.startLab()
+    updateStatus(nlab.labDirectory + " Lab ready!")
 
+def btnStopLab(nlab):
+    nlab.stopLab()
+    updateStatus(nlab.labDirectory + " Lab cleared!")
+
+def btnProbeLab(nlab):
+    updateStatus(nlab.labDirectory + " Probing...!")
+    nlab.probeLab()
 
 
 # Status bar -->-->-->-->-->-->-->-->-->-->
