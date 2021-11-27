16d58e75939d4b0392f0c5ef35d8cb78556d1e2d
Hex <33278795+CallumBasham@users.noreply.github.com>
2021-11-27 19:27:10 +0000

    Updated install script

--- install-run.sh
+++ install-run.sh
@@ -1,5 +1,8 @@
+# Install all required packages
 sudo apt-get install python3-pip
 sudo apt-get install xdotool
 sudo apt-get install python3-tk
 sudo apt-get install python3-scapy
 sudo pip install pyshark
+# Run the program
+python3 main.py
